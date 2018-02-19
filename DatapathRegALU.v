2e046d4291e2e09671b93cb3d1023baa14a069ca
Nate Hoffman <nhoffman15@me.com>
2018-02-19 14:54:08 -0500

    Handled change in ALU for order of inputs and outputs. Simplified mux. Removed extra DFF.

--- DatapathRegALU.v
+++ DatapathRegALU.v
@@ -1,42 +1,24 @@
-module DatapathRegALU(DA, SA, SB, W, reset, clock, K, BS, FS, write, selEN, status, data);
+module DatapathRegALU(DA, SA, SB, regW, reset, clock, K, BS, FS, ramW, selEN, status, data);
 	input [4:0] DA, SA, SB;
-	input W, reset, clock;
+	input reset, clock;
 	input [63:0]K;
 	input BS;
 	input [4:0] FS;
-	input write;
+	input regW, ramW;
 	input selEN;
 	output [3:0] status;
 	output [63:0] data;
 	
-	wire [63:0] A, BPre, BPost, dataALU, in, address;
+	wire [63:0] A, BPre, BPost, dataALU, dataRAM;
 	
-	RegFile32x64 regInst(A, BPre, data, DA, SA, SB, W, reset, clock);
+	RegFile32x64 regInst(data, DA, SA, SB, regW, reset, clock, A, BPre);
 	
 	ALU aluInst(A, BPost, FS, status, dataALU);
 	
-	Mux2to1 muxInst(BS, BPre, K, BPost);
+	assign BPost = BS ? K : BPre;
 	
-	RAM256x64 ramInst(address, clock, in, write, out);
+	RAM256x64 ramInst(dataALU, clock, in, ramW, dataRAM);
 	
 	assign data = selEN ? dataALU : dataRAM; // If selEN is true, data = dataALU; if selEN is false, data = dataRAM
-	
-	DFF inInst (.d(BPre), .clk(~clock), .clrn(reset), .prn(1'b1), .q(in));
-	DFF addressALUInst (.d(dataALU), .clk(~clock), .clrn(reset), .prn(1'b1), .q(address));
-	DFF outInst (.d(out), .clk(clock), .clrn(reset), .prn(1'b1), .q(dataRAM));
-	
-endmodule
-
-module Mux2to1(S, I0, I1, F);
-	input S;
-	input [63:0] I0, I1;
-	output reg [63:0]F;
-	
-	always @(*) begin
-		case(S)
-			1'b0: F <= I0;
-			1'b1: F <= I1;
-		endcase
-	end
 	
 endmodule
