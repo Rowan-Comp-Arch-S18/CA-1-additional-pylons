8627c3643915f8a2c8f60c3de848b4d490b8c79c
Cameron Bendzynski <bendzynsc4@students.rowan.edu>
2018-02-19 14:39:36 -0500

    Updated testbench for RAM implimentation

--- DatapathRegALUTestbench.v
+++ DatapathRegALUTestbench.v
@@ -4,16 +4,19 @@ module DatapathRegALUTestbench();
 	reg [63:0]K;
 	reg BS;
 	reg [4:0] FS;
+	reg selEN, write;
 	wire [3:0] status;
 	wire [63:0] data;
 	
-	DatapathRegALU dut(DA, SA, SB, W, reset, clock, K, BS, FS, status, data);
+	DatapathRegALU dut(DA, SA, SB, W, reset, clock, K, BS, FS, write, selEN, status, data);
 	
 	// R5 <= 24
 	// R7 <= 39
 	// R1 <= R5 + R7
 	// R30 <= R1 ^ R5
 	// R17 <= R30 << 2
+	// M[R7] <= R17
+	// R0 <= M[R7]
 	
 	initial begin
 		clock <= 1'b1;
@@ -48,6 +51,23 @@ module DatapathRegALUTestbench();
 		K <= 64'd2;
 		BS <= 1'b1;
 		FS <= 5'b10000;
+		#10 write <= 1'b1;
+		selEN <= 1'b0;
+		SA <= 5'd7;
+		SB <= 5'd17;
+		W <= 1'b0;
+		K <= 64'd0;
+		BS <= 1'b1;
+		FS <= 5'b00100;
+		#10 DA <= 5'd0;
+		write <= 1'b0;
+		selEN <= 1'b0;
+		SA <= 5'd30;
+		SB <= 5'd7;
+		W <= 1'b1;
+		K <= 64'd0;
+		BS <= 1'b1;
+		FS <= 5'b00100;
 		#5 $stop;
 	end
 	
