cf6e203845195b2c5dd6130d4a9c580027ecaa8f
bakerr5 <bakerr5@students.rowan.edu>
2018-02-19 13:25:23 -0500

    Comments for ALUTestbench

--- ALUTestbench.v
+++ ALUTestbench.v
@@ -1,26 +1,27 @@
 module ALUTestbench();
-	wire [63:0]F;
-	reg [63:0] A, B;
-	wire [3:0]status;
-	reg [4:0]FS;
+	reg [63:0] A, B; //64b inputs
+	reg [4:0] FS; //5b function select
 	
-	ALU dut(A, B, FS, status, F);
+	wire [3:0] status; //4b status
+	wire [63:0] F; //64b output
+	
+	ALU dut(A, B, FS, status, F); //design under test for ALU
 	
 	initial begin
-		FS <= 5'b00000;
-		A <= 64'd2;
-		B <= 64'd5;
-		#5 FS <= 5'b00100;
-		#5 FS <= 5'b01000;
-		A <= 64'd1;
-		B <= 64'd15;
-		#5 FS <= 5'b01001;
-		#5 FS <= 5'b10000;
-		#5 FS <= 5'b10100;
-		A <= 64'h8000000000000000;
-		#5 FS <= 5'b01100;
-		A <= 64'd3;
-		B <= 64'd6;
+		FS <= 5'b00000; //and, no invert
+		A <= 64'd2; //a gets decimal 2
+		B <= 64'd5; //b gets decimal 5
+		#5 FS <= 5'b00100; //or, no invert
+		#5 FS <= 5'b01000; //add, no invert
+		A <= 64'd1; //a gets decimal 1
+		B <= 64'd15; //b gets decimal 15
+		#5 FS <= 5'b01001; //add, b invert
+		#5 FS <= 5'b10000; //shift left
+		#5 FS <= 5'b10100; //shift right
+		A <= 64'h8000000000000000; // a gets hex 0
+		#5 FS <= 5'b01100; //xor, no invert
+		A <= 64'd3; //a gets decimal 3
+		B <= 64'd6; //b gets decimal 6
 		#5 $stop;
 	end
 endmodule
