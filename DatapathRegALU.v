9147102aad6634221e6db9789980c3bee45b8e39
Cameron Bendzynski <bendzynsc4@students.rowan.edu>
2018-03-19 14:15:36 -0400

    Datapath comments for clarity

--- DatapathRegALU.v
+++ DatapathRegALU.v
@@ -10,17 +10,17 @@ module DatapathRegALU(controlWord, reset, clock, K, status, data);
 	output [3:0] status;
 	output [63:0] data;
 	
-	assign {DA, SA, SB, FS, regW, ramW, selALU, selK} = controlWord;
+	assign {DA, SA, SB, FS, regW, ramW, selALU, selK} = controlWord; // assigning control signals to a single control word
 	
-	wire [63:0] A, BPre, BPost, dataALU, dataRAM;
+	wire [63:0] A, BPre, BPost, dataALU, dataRAM; // wires inbetween components, previously undefined
 	
-	RegFile32x64 regInst(data, DA, SA, SB, regW, reset, clock, A, BPre);
+	RegFile32x64 regInst(data, DA, SA, SB, regW, reset, clock, A, BPre); // instance of registry file
 	
-	ALU aluInst(A, BPost, FS, status, dataALU);
+	ALU aluInst(A, BPost, FS, status, dataALU); // instance of ALU
 	
-	assign BPost = selK ? K : BPre;
+	assign BPost = selK ? K : BPre; // MUX for selecting between B and K
 	
-	RAM256x64 ramInst(dataALU, clock, BPre, ramW, dataRAM);
+	RAM256x64 ramInst(dataALU, clock, BPre, ramW, dataRAM); // instance of RAM file
 	
 	assign data = selALU ? dataALU : dataRAM; // If selALU is true, data = dataALU; if selALU is false, data = dataRAM
 	
