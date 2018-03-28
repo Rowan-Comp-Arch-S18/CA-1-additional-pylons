484bf7ac0659eb66aa3067c813aa803b3602764f
Ian Nielsen <nielseni6@students.rowan.edu>
2018-03-28 18:57:51 -0400

    Tested with a testbench

--- DatapathRegALU.v
+++ DatapathRegALU.v
@@ -1,6 +1,7 @@
 module DatapathRegALU(controlWord, reset, clock, K, status, data);
-	input [23:0] controlWord;
+	input [25:0] controlWord;
 	wire [4:0] DA, SA, SB;
+	wire [1:0] PS;
 	input reset, clock;
 	input [63:0]K;
 	wire selK;
@@ -9,16 +10,11 @@ module DatapathRegALU(controlWord, reset, clock, K, status, data);
 	wire selALU;
 	output [3:0] status;
 	output [63:0] data;
-	wire [63:0] PC;
-	wire [31:0] ROMout;
-	wire [63:0] addOut, ALUresult, SHL2, sExtend, regInputB;
-	//wire [10:0] opCode;
+	wire [63:0] PC, PC4;
 	
 	
-	//assign opCode = ROMout[63:53];
-	
-	assign {DA, SA, SB, FS, regW, ramW, selALU, selK} = controlWord; // assigning control signals to a single control word
-	
+	assign {PS, DA, SA, SB, FS, regW, ramW, selALU, selK} = controlWord; // assigning control signals to a single control word
+
 	wire [63:0] A, BPre, BPost, dataALU, dataRAM; // wires inbetween components, previously undefined
 	
 	RegFile32x64 regInst(data, DA, SA, SB, regW, reset, clock, A, BPre); // instance of registry file
@@ -31,28 +27,8 @@ module DatapathRegALU(controlWord, reset, clock, K, status, data);
 	
 	assign data = selALU ? dataALU : dataRAM; // If selALU is true, data = dataALU; if selALU is false, data = dataRAM
 	
-	ROM romInst (ROMout, PC);
+	//add control unit
 	
-	Adder adderInst1 (PC, 64'd4, , , addOut); // addOut -> PS: 01
-	
-	Adder adderInst2 (addOut, regInputB, , , ALUresult); // ALUresult -> PS: 11
-	
-	Adder adderInst3 (SHL2, BPost, , , regInputB); // BPost -> PS: 10
-	
-	Shifter shifter_inst(sExtend, 6'd2, SHL2, );
-	
-	assign sExtend = {ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],
-		ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],
-		ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],
-		ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],
-		ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25],ROMout[25:0]};
-	
-	
-	
-	
-	//assign PC = ROMout[21] ? ALUresult : addOut;
-	
-												//  00,     01,    10,        11
-	Mux4to1Nbit muxInst1 (ROMout[21:20], PC, addOut, BPost, ALUresult, PC); 	
+	ProgramCounter(clock, controlWord, PS, PC, PC4);
 	
 endmodule
--- ProgramCounter.v
+++ ProgramCounter.v
@@ -1,17 +1,38 @@
-module ProgramCounter(PC, PC4, in, clock);
-	input [1:0] in;
-	input [63:0] PC;
+module ProgramCounter(clock, in, PS, PC, PC4);
+	input [1:0] PS;
 	input clock;
-	output reg [63:0] PC4;
+	input [25:0] in;
+	output [63:0] PC, PC4;
 	
-	always @(posedge clock) begin
-		case(in)
 	
-			00: PC4 <= PC;
-			01: PC4 <= PC + 4;
-			10: PC4 <= PC + 4 + PC;
-			11: PC4 <= PC + 4 + PC*4;
+	wire [63:0] PCi, PC;
+	reg [63:0] PCreg;
+	wire [31:0] ROMout;
+	wire [63:0] SHL2, signExtend, PC4PCx4;
 	
-		endcase
-	end
+	ROM romInst (ROMout, PC);
+	
+	Adder adderInst1 (PC, 64'd4, 1'b0, , PC4); 
+	
+	//Adder adderInst2 (addOut, PC4PCx4, 1'b0, , ALUresult);
+	
+	Adder adderInst3 (SHL2, PC4, 1'b0, , PC4PCx4); 
+	
+	Shifter shifter_inst(signExtend, 6'd2, SHL2, );
+	
+	assign signExtend = {{38{in[25]}},in[25:0]};
+	
+	//assign ROMextend = {{32{ROMout[31]}},ROMout[31:0]};
+	
+	//assign PC4 = addOut;
+	
+	always @(posedge clock)PCreg <= PCi;
+	
+	assign PC = PCreg;
+								 //  00,  01,  		 10,     11
+	Mux4to1Nbit muxInst1 (PS, PC, PC4, signExtend, PC4PCx4, PCi); 	
+	
+	
+	
+	
 endmodule
