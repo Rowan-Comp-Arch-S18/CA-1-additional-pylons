eeaa7dcf8516f3245f9bb36cd184328eaa3d27d8
DESKTOP-6JT1CS7\ryacu <bakerr5@students.rowan.edu>
2018-04-09 15:14:36 -0400

    Added Logic to R_ALU

--- R_ALU.v
+++ R_ALU.v
@@ -1,15 +1,16 @@
-module R_ALU(instruction, state, controlWord, nextState);
+module R_ALU(instruction, state, controlWord, nextState, K);
 	input [31:0] instruction;
 	input [1:0] state;
 	
 	output [28:0] controlWord;
 	output [1:0] nextState;
+	output [63:0] K;
 	
 	// SA = Rm, SB = Rn
 	
 	wire [1:0] Psel, Dsel;
 	wire [4:0] DA, SA, SB, Fsel;
-	wire regW, ramW, ALUsel, PCsel, SL;
+	wire regW, ramW, Bsel, PCsel, SL;
 	
 	assign Psel = 2'b01; //PC <- PC + 4
 	assign DA = instruction[4:0];
@@ -17,14 +18,16 @@ module R_ALU(instruction, state, controlWord, nextState);
 	assign SB = instruction[9:5];
 	assign regW = 1'b1; //Write to register
 	assign ramW = 1'b0; //Do not write to ram
-	assign ALUsel = 1'b0; //Pass B to ALU
-	assign Fsel = {3'b010, instruction[30], 1'b0}; //ADD, invert A on bit 30, do not invert B
+	assign Bsel = instruction[22] ? 1'b1 : 1'b0; //Pass K to ALU on bit 22
+	assign Fsel = {instruction[22], ((~instruction[28] & instruction[24]) | (instruction[30] & ~instruction[29] & ~instruction[28])), ((~instruction[27] & ~instruction[21]) | (~instruction[30] & instruction[29] & ~instruction[24]) | (instruction[30] & ~instruction[29] & ~instruction[24])), 1'b0, (instruction[30] & ~instruction[28] & instruction[24])}; //Just trust me
 	assign Dsel = 2'b01; //Enable ALU on data bus
 	assign PCsel = 1'b0; //Pass A to PC (This could be 0 or 1)
-	assign SL = instruction[29]; //Store on bit 29
+	assign SL = ((instruction[29] & instruction[24]) | (instruction[30] & instruction[29] & ~instruction[24])) ? 1'b1 : 1'b0; //Store on (bit 29 and bit 24) or (bit 30 and bit 29 and not bit 24)
 	
 	assign controlWord = {Psel, DA, SA, SB, Fsel, regW, ramW, Dsel, Bsel, PCsel, SL};
 	
 	assign nextState = 2'b00;
+	
+	assign K = {58'd0, instruction[15:10]};
 	
 endmodule
