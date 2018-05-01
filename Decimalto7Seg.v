12fe6957212816effb610ee0aaccbb4be714c6b4
Ian Nielsen <nielseni6@students.rowan.edu>
2018-05-01 02:12:36 -0400

    finished the GPIO with assembly code complete

--- Decimalto7Seg.v
+++ Decimalto7Seg.v
@@ -1,10 +1,26 @@
-module Decimalto7Seg(in, out);
-	
-	input [3:0] in;
+module Decimalto7Seg(S, in, out);
+
+	input [2:0]S;
+	input [31:0] in;
 	output reg [7:0] out;
 	
-	always @ (in) begin
-		case(in)
+	reg [3:0] into;
+	
+	always @(S) begin
+		case(S[2:0])
+			3'b000: into <= in[31:28];
+			3'b001: into <= in[27:24];
+			3'b010: into <= in[23:20];
+			3'b011: into <= in[19:16];
+			3'b100: into <= in[15:12];
+			3'b101: into <= in[11:8];
+			3'b110: into <= in[7:4];
+			3'b111: into <= in[3:0];
+			default: into <= 4'b0000;
+		endcase	
+	end
+	always @ (S) begin
+		case(into)
 			4'h0: out <= 8'b00111111;
 			4'h1: out <= 8'b00000110;
 			4'h2: out <= 8'b01011011;
@@ -15,6 +31,12 @@ module Decimalto7Seg(in, out);
 			4'h7: out <= 8'b00000111;
 			4'h8: out <= 8'b01111111;
 			4'h9: out <= 8'b01101111;
+			4'ha: out <= 8'b01110111;
+			4'hb: out <= 8'b01111100;
+			4'hc: out <= 8'b00111001;
+			4'hd: out <= 8'b01011110;
+			4'he: out <= 8'b01111001;
+			4'hf: out <= 8'b01110001;
 			default: out <= 8'b00000000;
 		endcase
 	end
--- GPIO.v
+++ GPIO.v
@@ -11,23 +11,24 @@ module GPIO(clock, GPIO1, button, switch, data, address, read, write, GPIO0);
 	output [31:0] GPIO0;
 	
 	reg [127:0] Screen_Data;
-	reg [3:0] SevSegData;
+	reg [31:0] SevSegData;
+	reg [63:0] counter;
 	
 	wire correctAddress;
 	wire [63:0] outData;
 	
-	assign outData = {{19'b0},{switch},{~button},{GPIO1}};
+	assign outData = (address[31:0] == 32'd4) ? counter : {{19'b0},{switch},{~button},{GPIO1}};
 	assign correctAddress = (address[63:56] == GPIOAddress);
 	assign data = (read & correctAddress) ? outData : 64'bz;
 	
-	always @ (negedge clock) begin
-		if (write) begin
-			if (address == 64'b0000010100000000000000000000000000000000000000000000000000000001) begin Screen_Data[63:0] <= data; end
-			if (address == 64'b0000010100000000000000000000000000000000000000000000000000000010) begin Screen_Data[127:64] <= data; end
-			if (address == 64'b0000010100000000000000000000000000000000000000000000000000000011) begin SevSegData <= data[3:0]; end
-		end
+	always @ (posedge clock) begin
+		counter <= counter + 1'b1;
+		if ((address == 64'b0000010100000000000000000000000000000000000000000000000000000001) & write) begin Screen_Data[63:0] <= data; end
+		if ((address == 64'b0000010100000000000000000000000000000000000000000000000000000010) & write) begin Screen_Data[127:64] <= data; end
 	end
-	
+	always @ (posedge clock) begin
+		if ((address == 64'b0000010100000000000000000000000000000000000000000000000000000011) & write) begin SevSegData <= data[31:0]; end
+	end
 	Screen_Render inst1(clock, SevSegData, Screen_Data[63:48], Screen_Data[47:32], 
 		Screen_Data[31:16], Screen_Data[15:0], Screen_Data[127:112], Screen_Data[113:96], 
 		Screen_Data[95:80], Screen_Data[79:64], GPIO0);
